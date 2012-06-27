94fd1257fefe8a7773aacfbbb04fd8ebc723d95f
Aaron Hedges <aaron@dashron.com>
2012-06-27 00:58:59 -0400

    add cookie output for the example resource

--- resources/example/example.desc.js
+++ resources/example/example.desc.js
@@ -11,6 +11,15 @@ module.exports = {
 			var resource = this.getResource('user');
 			resource.request('/users/1', view.child('user'));
 			resource.request('/users/5', view.child('user_two'));
+			
+			view.set('cookie_old_value', uri_bundle.cookie.get('date'));
+			var date = new Date().toString();
+			
+			uri_bundle.cookie.set('date', { value : date , domain : '127.0.0.1' });
+			uri_bundle.cookie.set('second_date', { value : date, domain : '127.0.0.1' });
+
+			view.set('cookie_new_value', date);
+
 			view.render('index.html');
 		},
 		options : {
