fbf104056968eddcf24fcb1740900934aebe2aa1
Aaron Hedges <aaron@dashron.com>
2012-08-01 14:27:19 -0400

    clean up code a bit

--- components/accept_header.js
+++ components/accept_header.js
@@ -6,7 +6,45 @@
 "use strict";
 
 /**
+ * Turns the accept header into an object.
+ * This object will have two properties, preferred and secondary.
+ * Both properties will contain an array of mime types.
+ * Preferred will be a list of mime types that the user wants, with no preferred order.
+ * Secondary will be an ordered list of mime types, from most preferred to least preferred.
+ * 
+ * @param  {String} accept
+ * @return {Object}
+ */
+var parseAcceptHeader = function (accept) {
+	// Identify the pattern of mime/type; q=#.#
+	var parse_regex = /([^;]+\/[^;]+)(; ?q=(\d\.\d))*/;
+	var i = 0;
+	var result = null;
+	var preferred_mime_types = [];
+	var secondary_mime_types = [];
+
+	if (typeof accept === "string") {
+		accept = accept.split(",");
+
+		for (i = 0; i < accept.length; i++) {
+			result = accept[i].match(parse_regex);
+			if (typeof result[3] === "undefined" || result[3] === null) {
+				preferred_mime_types[result[1]] = true;
+			} else {
+				secondary_mime_types.push({media_range : result[1], quality: result[3]});
+			}
+		}
+	}
+
+	return {
+		preferred: preferred_mime_types,
+		secondary: secondary_mime_types
+	};
+};
+
+/**
  * Returns the ideal content type from the accept string, which is also in your list of valid types
+ * 
  * @param  {string} accept          the accept header from the http request
  * @param  {Array} valid_types  an array of content types that you are willing to respond with
  * @return {string}                 a single content type
@@ -37,38 +75,10 @@ exports.getContentType = function (accept, valid_types) {
 		}
 	}
 
-	if (typeof valid_types[type_index] != "undefined" && valid_types[type_index] != null) {
+	if (typeof valid_types[type_index] !== "undefined" && valid_types[type_index] !== null) {
 		return valid_types[type_index];
 	}
 
 	//todo: replace with configurable default
 	return 'text/html';
-};
-
-// todo finish and fix to use standard http_request
-var parseAcceptHeader = function (accept) {
-	// Identify the pattern of mime/type; q=#.#
-	var parse_regex = /([^;]+\/[^;]+)(; ?q=(\d\.\d))*/;
-	var i = 0;
-	var result = null;
-	var preferred_mime_types = [];
-	var secondary_mime_types = [];
-
-	if (typeof accept == "string") {
-		accept = accept.split(",");
-
-		for (i = 0; i < accept.length; i++) {
-			result = accept[i].match(parse_regex);
-			if (typeof result[3] === "undefined" || result[3] === null) {
-				preferred_mime_types[result[1]] = true;
-			} else {
-				secondary_mime_types.push({media_range : result[1], quality: result[3]});
-			}
-		}
-	}
-
-	return {
-		preferred: preferred_mime_types,
-		secondary: secondary_mime_types
-	};
 };
