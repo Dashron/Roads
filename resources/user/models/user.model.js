e269b93185b98909a35d4bede0fa4ea7bc34dcdc
Aaron Hedges <aaron@dashron.com>
2012-08-01 23:27:27 -0400

    clean up tabs

--- resources/user/models/user.model.js
+++ resources/user/models/user.model.js
@@ -6,30 +6,30 @@ var Database = require('../../../components/database').Database;
 var UserModule = module.exports = new model_module.ModelModule();
 UserModule.connection = new Database('default');
 UserModule.setModel({
-        table : 'user',
-        fields : {
-                id : {
-                        type : 'id',
-                },
-                email : {
-                        type : 'email'
-                },
-                password : {
-                        type : 'password',
+	table : 'user',
+	fields : {
+		id : {
+			type : 'id',
+		},
+		email : {
+			type : 'email'
+		},
+		password : {
+			type : 'password',
 			set : function (password) {
 				this._password = crypto_module.createHash('sha256').update(password).digest('hex');
 			}
-                },
-                last_ip : {
-                        type : 'ip',
-                }
-        },
-        methods : {
-			checkPassword : function checkPassword(password) {
-				var sha256 = crypto_module.createHash('sha256').update(password);
-				return sha256.digest('hex') === this._password;
-			}
-        }
+		},
+		last_ip : {
+			type : 'ip',
+		}
+	},
+	methods : {
+		checkPassword : function checkPassword(password) {
+			var sha256 = crypto_module.createHash('sha256').update(password);
+			return sha256.digest('hex') === this._password;
+		}
+	}
 });
 
 UserModule.getAll = function () {
