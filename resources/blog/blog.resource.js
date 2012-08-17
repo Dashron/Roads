675aa6413f151d66d3322116f94aa99c67608e6c
Aaron Hedges <aaron@dashron.com>
2012-08-17 12:55:22 -0400

    remove incorrect dependency

--- resources/blog/blog.resource.js
+++ resources/blog/blog.resource.js
@@ -2,9 +2,6 @@ var Resource = require('../../components/resource').Resource;
 
 module.exports = new Resource('blog', {
 	router: require('./blog.router'),
-	dependencies : {
-		user : require('../user/user.resource')
-	},
 	properties : {
 		models : {
 			post : require('./models/post.model')
