a5c27a754e9ff2457bbddbb830a5a04bf4e1eefa
Aaron Hedges <aaron@dashron.com>
2015-06-01 22:48:28 -0400

    fix default errors for non-v8 browsers

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ module.exports.middleware = require('./lib/middleware');
 module.exports.HttpError = function HttpError(message, code) {
 	this.message = message;
 	this.code = code;
-	Error.captureStackTrace(this, HttpError);
+
+	if (Error.captureStackTrace) {
+		Error.captureStackTrace(this, HttpError);
+	} else {
+		// FF doesn't support captureStackTrace
+		this.stack = (new Error()).stack;
+	}
 };
 
 module.exports.HttpError.invalid_request = 400;
