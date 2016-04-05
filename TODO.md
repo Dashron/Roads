5908bcb0da51a9ed853255cc7d30d738adc2e48f
Aaron Hedges <aaron@dashron.com>
2016-04-04 22:52:25 -0400

    fix back button in pjax

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
-1. Fix PJAX back button
+1. Separate the opinionated router into middleware
 2. Add PJAX unit tests
 3. Add HttpServer unit tests
 4. Add koa and middleware examples
 5. Add code coverage
-6. Separate the opinionated router into middleware
--- src/client/pjax.js
+++ src/client/pjax.js
@@ -9,10 +9,9 @@ var cookie = require('cookie');
  */
 module.exports = class RoadsPjax {
 	/**
-	 * Creates a new RoadsPjax instance. You will still need to register the PJAX handler to your window if you 
+	 * Creates a new RoadsPjax instance. The road provided to this constructor will be the backbone of your PJAX requests.
 	 * 
-	 * @param  {[type]} road [description]
-	 * @return {[type]}      [description]
+	 * @param  {Road} road
 	 */
 	constructor(road) {
 		this._road = road;
@@ -37,11 +36,11 @@ module.exports = class RoadsPjax {
 	}
 
 	/**
-	 * [register description]
-	 * @param  {[type]} window            [description]
-	 * @param  {[type]} history           [description]
-	 * @param  {[type]} container_element [description]
-	 * @return {[type]}                   [description]
+	 * Registers the PJAX workflow to the provided window and container element. The container element will receive the contents
+	 * of the rendered HTML, and the window will be the source for all all history, redirection and dom interactions
+	 * 
+	 * @param  {Object} window
+	 * @param  {DomElement} container_element
 	 */
 	register (window, container_element) {
 		var _self = this;
@@ -54,30 +53,36 @@ module.exports = class RoadsPjax {
 
 		// Handle navigation changes besides pushState. TODO: don' blow out existing onpopstate's
 		window.onpopstate = function(event) {
-			_self._handleRoute(window.location.pathname, function (err, response) {
-				if (err) {
-					console.log('road err');
-					console.log(err);
-					return;
-				}
+			if (event.state.pjax) {
+				// if the popped state was generated  via pjax, execute the appropriate route
+				_self._handleRoute(window.location.pathname, function (err, response) {
+					if (err) {
+						console.log('road err');
+						console.log(err);
+						return;
+					}
 
-				container_element.innerHTML = response.body;
-				window.document.title = _self._page_title;
-			});
+					container_element.innerHTML = response.body;
+					window.document.title = _self._page_title;
+				});
+			} else {
+				// reload the page if the popped state wasn't generated via an pjax call
+				window.location.pathname = window.location.pathname;
+			}
 		};
 
 		// Trigger the pjax on any click event for roads links
 		container_element.addEventListener('click', _self._roadsLinkEvent.bind(_self, window, container_element));
 
 		// initial state
-		window.history.pushState({page_title: window.document.title}, this._page_title);
+		window.history.replaceState({page_title: window.document.title, pjax: false}, this._page_title);
 	}
 
 	/**
 	 * Make a client side roads request and render it to the page
-	 * @param  {[type]}   url      [description]
-	 * @param  {Function} callback [description]
-	 * @return {[type]}            [description]
+	 * @param  {string}   url
+	 * @param  {Function} callback
+	 * @return {Promise}
 	 */
 	_handleRoute (url, callback) {
 		return this._road.request('GET', url)
@@ -89,9 +94,11 @@ module.exports = class RoadsPjax {
 	}
 
 	/**
-	 * [_roadsLinkEvent description]
-	 * @param  {[type]} event [description]
-	 * @return {[type]}       [description]
+	 * Handles the link click event. If the link has the right data attribute we will execute and render the appropriate controller
+	 * 
+	 * @param  {Object} window            [description]
+	 * @param  {DomElement} container_element [description]
+	 * @param  {Object} event             [description]
 	 */
 	_roadsLinkEvent (window, container_element, event) {
 		if (event.target.tagName === 'A' && event.target.dataset.roads === "link" && !event.ctrlKey) {
@@ -105,7 +112,7 @@ module.exports = class RoadsPjax {
 					return;
 				}
 
-				window.history.pushState({page_title: _self._page_title}, _self._page_title, event.target.href);
+				window.history.pushState({page_title: _self._page_title, pjax: true}, _self._page_title, event.target.href);
 				container_element.innerHTML = response.body;
 				window.document.title = _self._page_title;
 			});
