f14e1e3cbbc5844e4f75d9bb26fdab71b982ebf3
Aaron Hedges <aaron@dashron.com>
2016-04-19 08:40:09 -0400

    add tests around new cookie header workflow

--- tests/middleware/testCookie.js
+++ tests/middleware/testCookie.js
@@ -6,7 +6,7 @@ var Response = require('../../index.js').Response;
 /**
  * Test that the cookies are properly parsed into the request context
  */
-exports.testCookieMiddlewareParsesCookiesIntoContext = function (test) {
+exports['test cookie middleware parses cookies into context'] = function (test) {
 	var context = {
 		Response: Response
 	};
@@ -29,7 +29,7 @@ exports.testCookieMiddlewareParsesCookiesIntoContext = function (test) {
 /**
  * Test that the cookie middleware provides easy to use functions on the Response object
  */
-exports.testCookieMiddlewareCanManipulateResponseCookies = function (test) {
+exports['test cookie middleware can manipulate response cookies'] = function (test) {
 	var context = {
 		Response: Response
 	};
@@ -38,9 +38,7 @@ exports.testCookieMiddlewareCanManipulateResponseCookies = function (test) {
 
 	test.ok(typeof(middleware) === 'function');
 
-	middleware.call(context, 'a', 'b', 'c', {
-		cookie: 'foo=bar;abc=def'
-	}, function () {
+	middleware.call(context, 'a', 'b', 'c', {}, function () {
 
 	});
 
@@ -58,6 +56,39 @@ exports.testCookieMiddlewareCanManipulateResponseCookies = function (test) {
 		abc: {
 			'value': 'def'
 		}
+	});
+
+	test.done();
+};
+
+
+/**
+ * Test that the cookie middleware successfully updates the response headers
+ */
+exports['test cookie middleware successfully updates the headers'] = function (test) {
+	var context = {
+		Response: Response
+	};
+
+	var middleware = cookie();
+
+	test.ok(typeof(middleware) === 'function');
+
+	middleware.call(context, 'a', 'b', 'c', {}, function () {});
+
+	var resp = new context.Response();
+
+	test.equal(typeof(resp.setCookie), 'function');
+	resp.setCookie('foo', 'bar');
+	resp.setCookie('abc', 'def');
+
+	test.equal(typeof(resp.getCookies), 'function');
+
+	test.deepEqual(resp.headers, {
+		'Set-Cookie': [
+			'foo=bar',
+			'abc=def'
+		]
 	});
 
 	test.done();
