b0a06b0950125f6e0c5dd616d260c8c59215b632
Aaron Hedges <aaron@dashron.com>
2020-03-31 19:30:19 -0400

    restore missing example files

---
+++ jsExample/routes/applyPrivateRoutes.js
@@ -0,0 +1,52 @@
+"use strict";
+/**
+ * applyPrivateRoutes.js
+ * Copyright(c) 2018 Aaron Hedges <aaron@dashron.com>
+ * MIT Licensed
+ * 
+ * This file is an example of how to assign some private routes to a road server
+ */
+
+var fs = require('fs');
+
+ /**
+  * Before calling this function you should create your roads object and bind a SimpleRouter to that road.
+  * You then pass the road to this function to assign a collection of example routes that should only
+  * be rendered on the server. 
+  * 
+  * @param {SimpleRouter} router - The router that the routes will be added to
+  */
+module.exports = function (router) {
+	router.addRoute('GET', '/private', function () {
+		this.setTitle('Private Resource');
+		var response = new this.Response('This is a private resource. It\'s available to the server, but is not build in the client! The landing page can be rendered via the client though, so try going back <a href="/" data-roads="link">home</a>!<br />');
+		response.setCookie('private_cookie', 'foo', {
+			httpOnly: true
+		});
+		
+		response.setCookie('public_cookie', 'bar', {
+			httpOnly: false
+		});
+
+		return response;
+	});
+
+	router.addRoute('GET', '/privateJSON', function () {
+		return new this.Response(JSON.stringify({"private-success": true}));
+	});
+
+	router.addRoute('GET', 'client.brws.js', function (url, body, headers) {
+		this.ignore_layout = true;
+		// In the real world the body of the response should be created from a template engine.
+		return new this.Response(fs.readFileSync(__dirname + '/../static/client.brws.js').toString('utf-8'), 200, {
+			'Content-Type': 'application/javascript; charset=UTF-8'
+		});
+	});
+
+	router.addRoute('GET', 'client.map.json', function (url, body, headers) {
+		// In the real world the body of the response should be created from a template engine.
+		return new this.Response(fs.readFileSync(__dirname + '/../static/client.map.json').toString('utf-8'), 200, {
+			'Content-Type': 'application/json; charset=UTF-8'
+		});
+	});
+};
---
+++ jsExample/routes/applyPublicRoutes.js
@@ -0,0 +1,52 @@
+"use strict";
+/**
+ * applyPublicRoutes.js
+ * Copyright(c) 2018 Aaron Hedges <aaron@dashron.com>
+ * MIT Licensed
+ * 
+ * This file is an example of how to assign some public routes to a roads server
+ */
+
+ /**
+  * Before calling this function you should create your roads object and bind a SimpleRouter to that road.
+  * You then pass the road to this function to assign a collection of example routes that will be rendered
+  * on both the client and the server
+  * 
+  * @param {SimpleRouter} router - The router that the routes will be added to
+  */
+module.exports = function (router) {
+	router.addRoute('GET', '/', function () {
+		this.setTitle('Root Resource');
+
+		// In the real world the body of the response should be created from a template engine.
+		return new this.Response(`Hello!<br />
+		 Try the <a href="/public" data-roads-pjax="link">public test link</a>.
+		 It's available to the server and can be rendered from the client! Try clicking it for the client path, or control clicking for the server.<br />
+		 Try the <a href="/private">private test link</a>. Itt's available to the server, but is not build in the client! Check your console for proof of the 404!`);
+	});
+
+	router.addRoute('GET', '/public', function () {
+		this.setTitle('Public Resource');
+		console.log('Here are all cookies accessible to this code: ', this.cookies);
+		console.log("Cookies are not set until you access the private route.");
+		console.log("Notice that the http only cookies do not show in your browser's console.log");
+
+		let html = `Hello!<br />
+		 The page you are looking at can be renderd via server or client. 
+		 The landing page can too, so try going back <a href="/" data-roads-pjax="link">home</a>!
+		 <form method="POST" action="/postdata" data-roads-pjax="form">
+			Message: <input type="text" name="message">
+			<input type="submit" value="Send message" data-roads-pjax="submit">
+		 </form>`;
+
+		// todo: make a client request to /privateJSON and get { "private-success": true }
+
+		return new this.Response(html);
+	});
+
+	router.addRoute('POST', '/postdata', function (url, body, headers) {
+		console.log('You sent the message:' + this.body.message);
+		this.ignore_layout = true;
+		return new this.Response('', 302, { location: '/public' });
+	});
+};
